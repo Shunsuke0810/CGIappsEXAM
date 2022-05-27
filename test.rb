a3c048b7fbaafaceaf694a44d28fe2622c9db9b8
SHUNSUKE <shunsuke0810@gmail.com>
2022-05-27 12:00:58 +0900

    CGIappsEXAM

---
+++ test.rb
@@ -0,0 +1,21 @@
+require 'webrick'
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb') 
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'exam.html.erb')
+server.mount('/goya_exam_1.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_exam_1.rb')
+server.mount('/goya_exam_2.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_exam_2.rb')
+
+
+server.start
